03b6fb75cf5e6d1ffd7aa043c1b7997c635d78dd
Shaun Valentine <svalentine@ten-x.com>
2022-10-25 15:32:00 -0700

    navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/navbar';
+import Home from './views/home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
