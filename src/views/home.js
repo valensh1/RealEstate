3a90b7ce104852591f9cb08443c78ea9e63acd7c
Shaun Valentine <svalentine@ten-x.com>
2022-10-26 21:35:11 -0700

    Navbar with main line

--- src/views/home.js
+++ src/views/home.js
@@ -2,9 +2,22 @@ import React from 'react';
 import Navbar from '../components/navbar';
 
 function home() {
+  const mainLine1 = 'THE SMARTER, FASTER, BETTER way';
+  const mainLine2 = 'to transact commercial real estate';
+
   return (
-    <div className="background-photo">
-      <Navbar />
+    <div id="home-container">
+      <div className="background-photo">
+        <Navbar />
+      </div>
+      <div className="main-line-container">
+        <h1 className="main-line" id="line1">
+          {mainLine1}
+        </h1>
+        <h1 className="main-line" id="line2">
+          {mainLine2}
+        </h1>
+      </div>
     </div>
   );
 }
