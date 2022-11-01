52c25132a2928902a24a7a03fec7fc3e0d43b69d
Shaun Valentine <valensh1@yahoo.com>
2022-10-31 18:32:14 -0700

    Featured Properties section update with additional listing cards

--- src/components/FeaturedProperties.js
+++ src/components/FeaturedProperties.js
@@ -76,6 +76,62 @@ function FeaturedProperties() {
             </div>
           </div>
         </div>
+        <div className="featured-properties-single-listing-card listing-cards">
+          <div className="featured-properties-listing-card-headings">
+            <h5>Online Auction</h5>
+            <h5>Oct 31 - Nov 2, 2022</h5>
+            <h5 className="property-type">Industrial</h5>
+          </div>
+          <div>
+            <a href="">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/SoFi_Stadium_2021.jpg"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="listing-card-bid-price">
+            <span>Starting Bid</span>
+            <span>$1,800,000</span>
+          </div>
+          <div className="listing-card-name-address">
+            <div className="featured-properties-listing-card-name listing-name">
+              <span>Industrial/Flex/Office | Brooklyn</span>
+            </div>
+            <div className="featured-properties-listing-address listing-address">
+              <span>20 VAN DAM ST,</span>
+              <span>BROOKLYN, NY 11222</span>
+            </div>
+          </div>
+        </div>
+        <div className="featured-properties-single-listing-card listing-cards">
+          <div className="featured-properties-listing-card-headings">
+            <h5>Online Auction</h5>
+            <h5>Oct 31 - Nov 2, 2022</h5>
+            <h5 className="property-type">Industrial</h5>
+          </div>
+          <div>
+            <a href="">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/SoFi_Stadium_2021.jpg"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="listing-card-bid-price">
+            <span>Starting Bid</span>
+            <span>$1,800,000</span>
+          </div>
+          <div className="listing-card-name-address">
+            <div className="featured-properties-listing-card-name listing-name">
+              <span>Industrial/Flex/Office | Brooklyn</span>
+            </div>
+            <div className="featured-properties-listing-address listing-address">
+              <span>20 VAN DAM ST,</span>
+              <span>BROOKLYN, NY 11222</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
